e49e465dfb01eaafd2e086b4086e83c038d170dc
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-02-14 15:13:45 +0300

    Reworked main class

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,12 @@ import utils.StringList;
 
 public class Main {
     public static void main(String[] args) {
-        StringList list = new StringList();
-        String string = "qwertyuiopaffffffffffffff";
-        list.append(string).append('1');
-        list.append(" what? ");
-        System.out.println(list);
-        System.out.println(list.length());
-        System.out.println(list.charAt(15));
-        //list.insert(10, "hello");
-        list.append('2').append('3');
-        System.out.println(list);
-
-        char[] temp = new char[16];
-        System.out.println(temp);
+        StringList stringList = new StringList();
+        stringList.append("test").append('1');
+        stringList.append(" hello").append(" world");
+        stringList.append('!');
+        stringList.insert(4, "test");
+        System.out.println(stringList);
+        System.out.println(stringList.substring(0, 18));
     }
 }
