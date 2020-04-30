c0c80c6ce5562fbf799d6f456c27795dca689864
Aleksei Vokhmin <vohmina2011@yandex.ru>
2020-04-30 16:12:39 +0300

    Fixed bugs and mistakes Added JavaDoc, deleted getIntValue, fixed equals and toString

--- src/utils/Fraction.java
+++ src/utils/Fraction.java
@@ -11,6 +11,13 @@ public class Fraction implements Comparable<Fraction> {
     private final int numerator;
     private final int denominator;
 
+    /**
+     * Конструктор рациональной броби
+     *
+     * @param numerator   числитель
+     * @param denominator знаменатель
+     * @throws WrongFractionException если числитель меньше 0 или знаменатель меньше или равен 0
+     */
     public Fraction(int numerator, int denominator) throws WrongFractionException {
         if (numerator < 0)
             throw new WrongFractionException("Numerator must be greater than 0!");
@@ -20,23 +27,44 @@ public class Fraction implements Comparable<Fraction> {
         this.denominator = denominator;
     }
 
+    /**
+     * Получить новый объект класса Fraction
+     *
+     * @param numerator   числитель
+     * @param denominator знаменатель
+     * @return новый объект класса
+     * @throws WrongFractionException если числитель меньше 0 или знаменатель меньше или равен 0
+     */
     public static Fraction valueOf(int numerator, int denominator) throws WrongFractionException {
         return new Fraction(numerator, denominator);
     }
 
-    public int getIntValue() {
-        return numerator / denominator;
-    }
-
+    /**
+     * Представление дроби в виде строки со сокращением.
+     * Если знаменатель равен 1, то убираем его.
+     * Если числитель больше знаменателя и если удаётся поделить бробь без остатка,
+     * то возвращаем сокращённое целочисленное число или возвращаем целую часть + остаток со знаменателем,
+     * иначе просто возвращаем дробь.
+     *
+     * @return дробь
+     */
     @Override
     public String toString() {
         if (numerator == 0) return "0";
+        // Сокращаем дробь, если числитель равен знаменателю
         if (numerator == denominator) return "1";
+        // Если знаменатель равен 1, то убираем его
+        if (denominator == 1) return String.valueOf(numerator);
+        // Если числитель больше знаменателя...
         if (numerator > denominator) {
+            // ...и если удаётся поделить бробь без остатка...
             if (numerator % denominator == 0)
-                return String.valueOf(getIntValue());
-            return numerator / denominator + " " + numerator % denominator + "/" + denominator;
+                // ...то возвращаем сокращённое целочисленное число...
+                return String.valueOf(numerator / denominator);
+            // ...или возвращаем целую часть + остаток со знаменателем.
+            return numerator / denominator + "+" + numerator % denominator + "/" + denominator;
         }
+        // ...иначе просто возвращаем дробь
         return numerator + "/" + denominator;
     }
 
@@ -45,8 +73,7 @@ public class Fraction implements Comparable<Fraction> {
         if (this == o) return true;
         if (!(o instanceof Fraction)) return false;
         Fraction fraction = (Fraction) o;
-        return numerator == fraction.numerator &&
-                denominator == fraction.denominator;
+        return this.compareTo(fraction) == 0;
     }
 
     @Override
